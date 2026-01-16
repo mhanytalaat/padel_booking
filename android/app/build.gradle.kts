b8d188694ed03a70d021a5e950126c341af553d1
mhanytalaat <m.hany.talaat@gmail.com>
2026-01-17 00:49:20 +0200

    Increment build number to 26

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         applicationId = "com.padelcore.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 25
+        versionCode = 26
         versionName = "1.1.2"
     }
 
