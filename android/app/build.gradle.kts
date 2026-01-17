8722e50b082a3269a466b86ef1e40391282cd6bf
mhanytalaat <m.hany.talaat@gmail.com>
2026-01-17 17:49:45 +0200

    Increment build number to 32 - Tournament dashboard and all configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         applicationId = "com.padelcore.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 31
+        versionCode = 32
         versionName = "1.1.2"
     }
 
